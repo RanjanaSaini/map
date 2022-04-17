842c8d306a9f0c8e93acc4528c35d06500891d40
Ranjana <01ranjana.saini@gmail.com>
2022-04-17 16:27:32 +0530

    changes

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    MY_KEY : 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
+  }
+    //  AIzaSyByJShPsfXZ9FMUOQx0S1I-L1XHZx4JEgY for geocoding
+    // AIzaSyAa_TmUynN_Wjm2OMCoNQNk2ITU1kcNTgw for place 
