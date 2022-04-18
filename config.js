3ed21f08f0d44f018889caded9771437f4cf4ea3
Ranjana Saini <01ranjana.saini@gmail.com>
2022-04-18 14:22:37 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    MY_KEY : 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
-  }
-    //  AIzaSyByJShPsfXZ9FMUOQx0S1I-L1XHZx4JEgY for geocoding
-    // AIzaSyAa_TmUynN_Wjm2OMCoNQNk2ITU1kcNTgw for place 
